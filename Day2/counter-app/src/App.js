0fe869a7038fe3a3867ec1ba7e6414f1d546ae90
Anupam Panwar <anupampanwar12@gmail.com>
2021-07-13 20:43:42 +0530

    Day2 task completed

---
+++ Day2/counter-app/src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const App = ({ name }) => {
+  let [score, setScore] = useState(0);
+  return (
+    <div className="main">
+      <h1 className="heading">Hello this is {name}</h1>
+      <h3>Welcome to my Counter Application</h3>
+      <div className="box">
+        <h2 className="score"> {score}</h2>
+        <div className="controls">
+          <button
+            className="btn"
+            onClick={() => {
+              score < 25 ? setScore(score + 1) : setScore(0);
+            }}
+          >
+            {" "}
+            Increase{" "}
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              setScore(0);
+            }}
+          >
+            {" "}
+            Reset{" "}
+          </button>
+          <button
+            className="btn"
+            onClick={() => {
+              score > 0 ? setScore(score - 1) : setScore(0);
+            }}
+          >
+            {" "}
+            Decrease{" "}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
